2cc4a9f50c56b293fe0b6ce87507c186671a79e4
Edison Guo <hydra1983@gmail.com>
2014-12-14 22:40:49 +0800

    First Commit

---
+++ docker-enter.plugin.zsh
@@ -0,0 +1,3 @@
+function docker-enter { 
+	boot2docker ssh -t "[ -f /var/lib/boot2docker/nsenter ] || docker run --rm -v /var/lib/boot2docker/:/target jpetazzo/nsenter; sudo /var/lib/boot2docker/docker-enter $@"; 
+}
